db879acf48e616071c399a137d176f2e0eb4428c
Kenny Kerr <kenny@kennykerr.ca>
2024-04-11 15:08:57 -0500

    works

--- crates/tests/interface_core/tests/borrow.rs
+++ crates/tests/interface_core/tests/borrow.rs
@@ -2,15 +2,15 @@
 
 use windows_core::*;
 
-pub const E_POINTER: HRESULT = HRESULT(0x80004003_u32 as _);
 pub const S_OK: HRESULT = HRESULT(0);
+pub const E_INVALIDARG: HRESULT = HRESULT(0x80070057_u32 as _);
+pub const E_POINTER: HRESULT = HRESULT(0x80004003_u32 as _);
 
 #[interface("09428a59-5b40-4e4c-9175-e7a78514316d")]
 unsafe trait ITest: IUnknown {
-    unsafe fn nothing(&self);
-    unsafe fn hresult(&self) -> HRESULT;
     unsafe fn usize(&self, input: usize, output: *mut usize) -> HRESULT;
     unsafe fn hstring(&self, input: Borrow<HSTRING>, output: *mut HSTRING) -> HRESULT;
+    unsafe fn interface(&self, input: Borrow<ITest>, output: *mut Option<ITest>) -> HRESULT;
 }
 
 // TODO: interface macro should generate callside methods with AsBorrow traits?
@@ -19,10 +19,6 @@ unsafe trait ITest: IUnknown {
 struct Test;
 
 impl ITest_Impl for Test {
-    unsafe fn nothing(&self) {}
-    unsafe fn hresult(&self) -> HRESULT {
-        S_OK
-    }
     unsafe fn usize(&self, input: usize, output: *mut usize) -> HRESULT {
         if output.is_null() {
             return E_POINTER;
@@ -37,14 +33,22 @@ impl ITest_Impl for Test {
         *output = input.clone();
         S_OK
     }
+    unsafe fn interface(&self, input: Borrow<ITest>, output: *mut Option<ITest>) -> HRESULT {
+        if input.is_none() {
+            return E_INVALIDARG;
+        }
+        if output.is_null() {
+            return E_POINTER;
+        }
+        *output = input.clone();
+        S_OK
+    }
 }
 
 #[test]
 fn test() {
     unsafe {
         let test: ITest = Test.into();
-        test.nothing();
-        assert_eq!(test.hresult(), S_OK);
 
         assert_eq!(test.usize(0, std::ptr::null_mut()), E_POINTER);
         let mut output = 0;
@@ -55,5 +59,12 @@ fn test() {
         let mut output = HSTRING::new();
         assert_eq!(test.hstring(h!("hello"), &mut output), S_OK);
         assert_eq!(&output, h!("hello"));
+
+        assert_eq!(test.interface(None, std::ptr::null_mut()), E_INVALIDARG);
+        assert_eq!(test.interface(&test, std::ptr::null_mut()), E_POINTER);
+        let mut output = None;
+        assert_eq!(test.interface(&test, &mut output), S_OK);
+        assert_eq!(test.interface(Some(&test), &mut output), S_OK);
+        assert_eq!(output.as_ref(), Some(&test));
     }
 }
